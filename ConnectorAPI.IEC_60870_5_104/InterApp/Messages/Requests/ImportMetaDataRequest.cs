5210005448e7e59d5642fb72023904493cb5dbb4
Samuel Duarte <samuel.duarte@skyline.be>
2025-10-20 14:14:21 +0100

    Add request Guid to ImportMetaDataRequest

--- ConnectorAPI.IEC_60870_5_104/InterApp/Messages/Requests/ImportMetaDataRequest.cs
+++ ConnectorAPI.IEC_60870_5_104/InterApp/Messages/Requests/ImportMetaDataRequest.cs
@@ -1,5 +1,6 @@
 ﻿namespace Skyline.DataMiner.ConnectorAPI.IEC_60870_5_104.InterApp.Messages.Requests
 {
+	using System;
 	using System.Collections.Generic;
 
 	using Skyline.DataMiner.ConnectorAPI.IEC_60870_5_104.Models;
@@ -13,5 +14,10 @@
 		/// Gets or sets the collection of metadata rows to be imported.
 		/// </summary>
 		public List<MetaDataRow> MetaDataRows { get; set; }
+
+		/// <summary>
+		/// Gets or sets the unique identifier for this import request, allowing correlation with the import script execution from the element.
+		/// </summary>
+		public Guid? RequestGuid { get; set; }
 	}
 }
